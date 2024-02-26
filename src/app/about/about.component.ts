582184df02649a045dfcba68c84d373b476097ed
Vats4u2u <pranshuvats232@gmail.com>
2024-02-26 16:23:12 +0530

    my next

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit } from '@angular/core';
+declare var $: any;
 
 @Component({
-  selector: 'app-about',
-  templateUrl: './about.component.html',
-  styleUrl: './about.component.css'
+  selector: 'app-about', // Update the selector
+  templateUrl: './about.component.html', // Rename the template file accordingly
+  styleUrls: ['./about.component.css'] // Rename the style file accordingly
 })
-export class AboutComponent {
+export class AboutComponent implements AfterViewInit {
 
+  ngAfterViewInit(): void {
+    const elem = $("#parallax");
+
+    // Add event listener
+    $(document).on("mousemove", (e: MouseEvent) => {
+      this.parallax(e, elem);
+    });
+  }
+
+  // Magic happens here
+  private parallax(e: MouseEvent, elem: any): void {
+    let _w = window.innerWidth / 2;
+    let _h = window.innerHeight / 2;
+    let _mouseX = e.clientX;
+    let _mouseY = e.clientY;
+    let _depth1 = `${50 - (_mouseX - _w) * 0.01}% ${50 - (_mouseY - _h) * 0.01}%`;
+    let _depth2 = `${50 - (_mouseX - _w) * 0.02}% ${50 - (_mouseY - _h) * 0.02}%`;
+    let _depth3 = `${50 - (_mouseX - _w) * 0.06}% ${50 - (_mouseY - _h) * 0.06}%`;
+    let x = `${_depth3}, ${_depth2}, ${_depth1}`;
+    console.log(x);
+    elem.css("background-position", x);
+  }
 }
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,16 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit } from '@angular/core';
+declare var $: any;
 
-declare function parallax():any;
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
-
-
+  styleUrls: ['./home.component.css']
 })
-export class HomeComponent {
+export class HomeComponent implements AfterViewInit {
 
-  ngOnInit(){
-    parallax();
+  ngAfterViewInit(): void {
+    const elem = $("#parallax");
+
+    // Add event listener
+    $(document).on("mousemove", (e: MouseEvent) => {
+      this.parallax(e, elem);
+    });
+  }
+
+  // Magic happens here
+  private parallax(e: MouseEvent, elem: any): void {
+    let _w = window.innerWidth / 2;
+    let _h = window.innerHeight / 2;
+    let _mouseX = e.clientX;
+    let _mouseY = e.clientY;
+    let _depth1 = `${50 - (_mouseX - _w) * 0.01}% ${50 - (_mouseY - _h) * 0.01}%`;
+    let _depth2 = `${50 - (_mouseX - _w) * 0.02}% ${50 - (_mouseY - _h) * 0.02}%`;
+    let _depth3 = `${50 - (_mouseX - _w) * 0.06}% ${50 - (_mouseY - _h) * 0.06}%`;
+    let x = `${_depth3}, ${_depth2}, ${_depth1}`;
+    console.log(x);
+    elem.css("background-position", x);
   }
 }
